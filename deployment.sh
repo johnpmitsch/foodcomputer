2ecd53974155589812b889c0c002d7d66c82190b
John Mitsch <jomitsch@redhat.com>
2019-02-09 17:45:10 -0500

    Added deployment script

---
+++ deployment.sh
@@ -0,0 +1,8 @@
+FC_DIR="/root/foodcomputer-server"
+export NODE_ENV=production
+
+cd $FC_DIR
+git fetch --all
+git reset --hard origin/master
+npm install
+$FC_DIR/node_modules/.bin/sequelize db:migrate

748b7f7effff95ea535217c18a4bb53fadd47089
John Mitsch <jomitsch@redhat.com>
2019-02-10 15:22:15 -0500

    Added pm2 settings

---
+++ pm2-production.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'foodcomputer-api',
+    script: 'bin/www',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};

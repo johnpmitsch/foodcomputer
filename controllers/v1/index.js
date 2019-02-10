08fbc04ec0db30dc55fa5774dd3d15f3a15cd14b
John Mitsch <jomitsch@redhat.com>
2019-02-09 21:40:02 -0500

    Testing ping

--- controllers/v1/index.js
+++ controllers/v1/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/ping", (_req, res, _next) => {
-  res.status(200).send({ ping: "Welcome to foodcomputer.io!" });
+  res.status(200).send({ ping: "Hello! Welcome to foodcomputer.io!" });
 });
 
 module.exports = router;
